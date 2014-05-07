f7bd5e98da53cd8e75a1e7e13e06b2f67381310e
Simon.ZH <dreamzsm@gmail.com>
2014-05-07 15:11:28 +0800

    开始做最粗的界面

--- GtAppDemo/src/com/geetest/gtappdemo/MainActivity.java
+++ GtAppDemo/src/com/geetest/gtappdemo/MainActivity.java
@@ -14,13 +14,14 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.gtappdemo);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, new PlaceholderFragment())
+//                    .commit();
+//        }
     }
 
 
